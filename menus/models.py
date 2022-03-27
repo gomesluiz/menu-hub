6e906dbcfd52a056a36577e45a0134537d27a941
gomesluiz <gomes.luiz@gmail.com>
2022-03-27 18:10:36 -0300

    includes database migrations

--- menus/models.py
+++ menus/models.py
@@ -1,3 +1,27 @@
+from pyexpat import model
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+
+
+class Restaurant(models.Model):
+    """Um objeto da classse Restaurant armazena informaçoes sobre um 
+       restaurante."""
+       
+    name = models.CharField(max_length=45)
+
+    def __str__(self):
+        return f"[Restaurant] name: {self.name}"
+
+
+class MenuItem(models.Model):
+    """Um objeto da classse MenuItem armazena informaçoes sobre um 
+       item de um menu de um restaurante."""
+       
+    category = models.CharField(max_length=45)
+    description = models.CharField(max_length=250)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"[MenuItem] description: {self.description[:50]}"
